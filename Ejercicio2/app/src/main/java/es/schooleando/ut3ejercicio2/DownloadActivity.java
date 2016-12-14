0fb6ea0b45f58189da22770d962561d569feb38b
Jose <josegramage@gmail.com>
2016-12-14 23:11:13 +0100

    Ejercicio 2 Jose Gramage

--- Ejercicio2/app/src/main/java/es/schooleando/ut3ejercicio2/DownloadActivity.java
+++ Ejercicio2/app/src/main/java/es/schooleando/ut3ejercicio2/DownloadActivity.java
@@ -5,9 +5,46 @@ import android.os.Bundle;
 
 public class DownloadActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download);
-    }
+	TextView txt_url;
+	ImageView imagen;
+	Button btn_descargar;
+	ProgressBar progreso;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_download);
+
+		imagen = (ImageView) findViewById(R.id.imageView);
+		btn_descargar = (Button) findViewById(R.id.button);
+		txt_url = (TextView) findViewById(R.id.textURL);
+		progreso = (ProgressBar) findViewById(R.id.progressBar);
+
+		btn_descargar.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				String stringUrl = txt_url.getText().toString();
+				ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+				if (networkInfo != null && networkInfo.isConnected()) {
+					new DownloadURLTask().execute(stringUrl);
+
+				} else {
+					txt_url.setText("No network connection available.");
+
+				}
+
+			}
+
+		});
+
+		/*
+		 * @Override public void onImageLoaded (Bitmap bitmap){
+		 * 
+		 * imagen.setImageBitmap(bitmap); }
+		 */
+	}
+
 }
--- Ejercicio2/app/src/main/java/es/schooleando/ut3ejercicio2/DownloadURLTask.java
+++ Ejercicio2/app/src/main/java/es/schooleando/ut3ejercicio2/DownloadURLTask.java
@@ -7,15 +7,31 @@ import android.os.AsyncTask;
  */
 
 public class DownloadURLTask extends AsyncTask {
+	//public class DownloadURLTask extends AsyncTask<String, Void, String> {
+
+    private static final String DEBUG_TAG = "HttpExample";
+
+    ProgressDialog progressDialog;
+
+
     @Override
     protected Object doInBackground(Object[] params) {
-        return null;
+        try {
+            int lenght = tryGetFileSize(params[0].toString());
+            Log.i(DEBUG_TAG, "lenght = " + String.valueOf(lenght));
+
+            return downloadUrl(params[0].toString());
+        } catch (IOException e) {
+            return "Unable to retrieve web page. URL may be invalid.";
+        }
     }
+
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
     }
+
 
     @Override
     protected void onPostExecute(Object o) {
@@ -26,4 +42,66 @@ public class DownloadURLTask extends AsyncTask {
     protected void onProgressUpdate(Object[] values) {
         super.onProgressUpdate(values);
     }
+
+
+
+    private int tryGetFileSize(String _url) {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(_url);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.getInputStream();
+            return conn.getContentLength();
+        } catch (IOException e) {
+            return -1;
+        } finally {
+            conn.disconnect();
+        }
+    }
+
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+
 }
+
